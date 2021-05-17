dde7397e7d9dc183f68d27b6be24826a3af16849
ParkMH <46748131+M16Lukas@users.noreply.github.com>
2021-05-17 22:20:09 +0900

    Create Computer.java

---
+++ lesson3/src/chapter1/Inheritance1/Computer.java
@@ -0,0 +1,77 @@
+package chapter1.Inheritance1;
+
+/**
+ * Computer
+ */
+public class Computer {
+  // Field
+  protected String manufacturer;
+  protected String processor;
+  protected int ramSize;
+  protected int diskSize;
+  protected double processorSpeed;
+
+  // Constructor
+  public Computer(String manufacturer, String processor, int ramSize, int diskSize, double processorSpeed) {
+    this.manufacturer = manufacturer;
+    this.processor = processor;
+    this.ramSize = ramSize;
+    this.diskSize = diskSize;
+    this.processorSpeed = processorSpeed;
+  }
+
+  // Getter & Setter
+  public String getManufacturer() {
+    return manufacturer;
+  }
+
+  public void setManufacturer(String manufacturer) {
+    this.manufacturer = manufacturer;
+  }
+
+  public String getProcessor() {
+    return processor;
+  }
+
+  public void setProcessor(String processor) {
+    this.processor = processor;
+  }
+
+  public int getRamSize() {
+    return ramSize;
+  }
+
+  public void setRamSize(int ramSize) {
+    this.ramSize = ramSize;
+  }
+
+  public int getDiskSize() {
+    return diskSize;
+  }
+
+  public void setDiskSize(int diskSize) {
+    this.diskSize = diskSize;
+  }
+
+  public double getProcessorSpeed() {
+    return processorSpeed;
+  }
+
+  public void setProcessorSpeed(double processorSpeed) {
+    this.processorSpeed = processorSpeed;
+  }
+
+  // Override Method
+  @Override
+  public String toString() {
+    String result = "manufacturer : " + manufacturer + "\nprocessor : " + processor + "\nramSize : " + ramSize
+        + "\ndiskSize : " + diskSize + "\nprocessorSpeed : " + processorSpeed;
+
+    return result;
+  }
+
+  // Method
+  public double computePower() {
+    return ramSize * processorSpeed;
+  }
+}
