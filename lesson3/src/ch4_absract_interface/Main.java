b27ee160f42b66de7e451faac660e47043be2a0a
ParkMH <46748131+M16Lukas@users.noreply.github.com>
2021-05-21 18:01:45 +0900

    Create Main.java

---
+++ lesson3/src/ch4_absract_interface/Main.java
@@ -0,0 +1,12 @@
+package ch4_absract_interface;
+
+/**
+ * Main
+ */
+public class Main {
+  // 추상 클래스(abstract class)
+
+  // 1. 추상 메서드 : 선언만 있고 구현이 없는 메서드
+  // 2. 추상 클래스 : 추상 메서드를 포함한 클래스
+  // 3. 키워드는 abstract 로 표시
+  // 4. 추상 클래스는 객체를 만들수없음. 따라서 서브 클래스를 만드는 용도로만 사용됨
